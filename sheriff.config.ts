db327b5bb321fe3d8cc3b6187ce123ac2af6d7be
p-iknow <apricotsoul@gmail.com>
2026-05-16 18:51:23 +0900

    refactor: remove compat apps

--- sheriff.config.ts
+++ sheriff.config.ts
@@ -5,16 +5,10 @@ export const config: SheriffConfig = {
 
   entryPoints: {
     docs: './apps/docs/src/pages/index.astro',
-    'compat-react-17': './apps/compat-react-17/src/compat.test.ts',
-    'compat-react-18': './apps/compat-react-18/src/compat.test.ts',
-    'compat-react-19': './apps/compat-react-19/src/compat.test.ts',
   },
 
   modules: {
     'apps/docs/src': 'app:docs',
-    'apps/compat-react-17/src': 'app:compat',
-    'apps/compat-react-18/src': 'app:compat',
-    'apps/compat-react-19/src': 'app:compat',
     'packages/react-hook-kit/src': 'lib:react-hook-kit',
   },
 
--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,6 +1,1 @@
-export default [
-  'packages/react-hook-kit',
-  'apps/compat-react-17',
-  'apps/compat-react-18',
-  'apps/compat-react-19',
-]
+export default ['packages/react-hook-kit']
